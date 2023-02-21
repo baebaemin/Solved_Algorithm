dc49f6e9b5714c6e97e9c974c9e0de3d1fca07a6
Minkyoung Bae <hellobaemin@gmail.com>
2023-02-21 17:35:44 +0900

    [Silver I] Title: 단지번호붙이기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,32 @@
+def BFS(r, c):
+    Q = [(r, c)]
+    arr[r][c] = 0
+    visited[r][c] = 1
+    cnt = 1
+
+    while Q:
+        r, c = Q.pop(0)
+        for dr, dc in ((-1, 0), (0, 1), (1, 0), (0, -1)):
+            nr, nc = r+dr, c+dc
+            if 0 <= nr < N and 0 <= nc < N and arr[nr][nc] == 1 and not visited[nr][nc]:
+                visited[nr][nc] = 1
+                arr[nr][nc] = 0
+                cnt += 1
+                Q.append((nr, nc))
+    return cnt
+
+
+N = int(input())
+arr = [[int(i) for i in input()] for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
+
+rlt = 0
+lst = []
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == 1:
+            lst.append(BFS(i, j))
+            rlt += 1
+print(rlt)
+for i in range(len(lst)):
+    print(sorted(lst)[i])
