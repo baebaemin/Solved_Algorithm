0001e9cdf6cb3f37a72095aa54d28efe25a61e90
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-03 07:02:37 +0900

    [Silver V] Title: 수 이어가기, Time: 88 ms, Memory: 36372 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.py
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.py
@@ -1,22 +1,20 @@
-def conti_num(A, B, list):
+def BF(A, B, list):
     global maxV
     if B < 0:
-        if len(list)-1 > maxV:
-            maxV = len(list)-1
-        Q.append([len(list)-1, list[:-1]])
+        if len(list) > maxV:
+            maxV = len(list)
+        Q.append(list[:-1])
         return
-    conti_num(B, A-B, list + [A-B])
+    BF(B, A-B, list + [A-B])
 
 N = int(input())
-num_list = [i for i in range(N+1)]
 maxV = -1
 Q = []
-for num in num_list:
-    if 0 < num <= N:
-        conti_num(N, num, [N, num])
+for num in range(1, N + 1):
+    BF(N, num, [N, num])
 
-for i in range(len(num_list)):
-    if Q[i][0] == maxV:
-        print(maxV)
-        print(*Q[i][1])
+for i in range(N):
+    if len(Q[i])+1 == maxV:
+        print(maxV-1)
+        print(*Q[i])
         break
