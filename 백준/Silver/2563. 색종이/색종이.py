4827659f23facf19f6b9932e322d7edd292c456d
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-03 22:52:49 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,29 +1,11 @@
-def mapping(lst, min, idx):
-    for j in range(N):
-        new[j][idx] = lst[j] - min
-
 N = int(input())
-left = [0] * N
-up = [0] * N
-new = [[0, 0] for _ in range(N)]
-
-for i in range(N):
-    l, u = map(int, input().split())
-    left[i], up[i] = l, u
-
-L, U = min(left), min(up)
-ML, MU = max(left) + 10, max(up) + 10
-arr = [[0] * ML for _ in range(MU)]
-
-mapping(left, L, 1)
-mapping(up, U, 0)
-
-ans = 0
-for k in range(N):
-    R, C = new[k][0], new[k][1]
-    for r in range(R, R+10):
-        for c in range(C, C+10):
-            if not arr[r][c]:
-                arr[r][c] = 1
-                ans += 1
-print(ans)
+arr = [[0] * 100 for _ in range(100)]
+cnt = 0
+for _ in range(N):
+    N, M = map(int, input().split())
+    for n in range(N, N+10):
+        for m in range(M, M+10):
+            if not arr[n][m]:
+                arr[n][m] = 1
+                cnt += 1
+print(cnt)
