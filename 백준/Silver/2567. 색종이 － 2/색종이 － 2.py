fb58bad66e7a704f94fb0140f5438b829f5e6d20
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-03 22:59:16 +0900

    [Silver IV] Title: 색종이 - 2, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,3 +1,13 @@
+def paper(d_arr):
+    global rlt
+    for r in range(102):
+        if sum(d_arr[r]) != 0:
+            code = []
+            for idx in range(101):
+                if d_arr[r][idx] != d_arr[r][idx + 1]:
+                    code.append(d_arr[r][idx])
+            rlt += sum(code) * 2
+
 N = int(input())
 arr = [[0] * 102 for _ in range(102)]
 for _ in range(N):
@@ -6,24 +16,7 @@ for _ in range(N):
         for m in range(M, M+10):
             arr[n][m] = 1
 
-line = []
 rlt = 0
-for r in range(102):
-    if sum(arr[r]) != 0:
-        code = []
-        if arr[r] == 1:
-            rlt += 2
-        for idx in range(101):
-            if arr[r][idx] != arr[r][idx+1]:
-                code.append(arr[r][idx])
-        rlt += sum(code)*2
-
-arr_t = list(zip(*arr))
-for c in range(102):
-    if sum(arr_t[c]) != 0:
-        code = []
-        for idx in range(101):
-            if arr_t[c][idx] != arr_t[c][idx+1]:
-                code.append(arr_t[c][idx])
-        rlt += sum(code)*2
+paper(arr)
+paper(list(zip(*arr)))
 print(rlt)
