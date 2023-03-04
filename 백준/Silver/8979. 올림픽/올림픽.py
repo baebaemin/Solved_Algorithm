a13190f2157b3402f2d7aba8022d28280b3cbf53
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-04 19:39:17 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,14 +3,15 @@ N, K = map(int, input().split())
 lst = [0] * N
 for i in range(N):
     n, G, S, B = map(int, sys.stdin.readline().split())
-    lst[n-1] = (G*3 + S*2 + B*1, n*-1)
-item, score = lst[K-1], lst[K-1][0]
-lst = [(-1, 0)] + sorted(lst, reverse=True)
+    lst[n-1] = (G, S, B, n)
+
+item, score = lst[K-1], lst[K-1][:3]
+lst = [(0, 0, 0, 0)] + sorted(lst, reverse=True)
 rlt = idx = lst.index(item)
 cnt = 0
 while 1:        # 앞에 동일 등수 국가가 있는지 찾기
     idx -= 1
-    if lst[idx][0] != score:
+    if lst[idx][:3] != score:
         break
     cnt += 1
 print(rlt - cnt)
