500a3f188503ad93dfbbd60b78840db323c3c32b
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-05 22:06:18 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,6 +1,3 @@
 N = int(input())
-for i in range(N//5, -1, -1):
-    if (N-5*i) % 3 == 0:
-        print(i + (N - 5 * i) // 3)
-        break
-else: print('-1')
+rlt = [i + (N - 5 * i) // 3 for i in range(N//5, -1, -1) if (N-5*i) % 3 == 0]
+print(rlt[0] if rlt else -1)
