a8aa222e5319b257d64920d370f6ef75f15ed3fd
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-05 14:54:52 +0900

    [Silver II] Title: 참외밭, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2477. 참외밭/참외밭.py
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -1,10 +1,9 @@
 N = int(input())
 d_lst = [0] * 6
 lst = [0] * 6
+
 for i in range(6):
-    d, n = tuple(map(int, input().split()))
-    d_lst[i] = d
-    lst[i] = n
+    d_lst[i], lst[i] = tuple(map(int, input().split()))
 for j in range(6):
     if d_lst[j % 6] == d_lst[(j+2) % 6] and d_lst[(j+1) % 6] == d_lst[(j+3) % 6]:
         break
