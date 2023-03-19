3227ec981373654bee9f7905d68135868feb2364
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-19 10:53:19 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 404 ms, Memory: 179356 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,14 @@
 import sys
-from collections import deque
 
-def BFS(n, cnt):
+def DFS(n):
     if v[n]:
         return
-    Q = deque([n])
-    v[n] = cnt
-    while Q:
-        n = Q.popleft()
-        for m in adjL[n]:
-            if not v[m]:
-                v[m] = cnt
-                Q.append(m)
-
-    for i in range(1, N+1):
-        if not v[i]:
-            BFS(i, cnt+1)
-            break
-    else:
-        print(cnt)
+    v[n] = 1
+    for m in adjL[n]:
+        DFS(m)
 
 input = sys.stdin.readline
-N, M = map(int, input().split())     # 정점의 개수 간선의 개수
+N, M = map(int, input().split())  # 정점의 개수 / 간선의 개수
 adjL = [[] for _ in range(N+1)]
 v = [0] * (N + 1)
 
@@ -30,8 +17,10 @@ for _ in range(M):
     adjL[t].append(u)
     adjL[u].append(t)
 
-if M == 0:
-    print(N)
-    exit()
+cnt = 0
+for i in range(1, N+1):
+    if not v[i]:
+        DFS(i)
+        cnt += 1
 
-BFS(t, 1)
+print(cnt)
