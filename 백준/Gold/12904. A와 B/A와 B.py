4faa418665ca518d882665ad0ce56df26525abe0
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-25 16:07:26 +0900

    [Gold V] Title: A와 B, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -1,33 +1,22 @@
-from collections import deque
-
-def check(S, T):  # S = AB / T = ABB
-    for _ in range(len(T) - len(S)):  # 3 - 2 = 1, 1번 반복할것임 2
-        while Q:
-            T = Q.popleft()
+def check(S, T):
+    while Q:
+        T = Q.pop(0)
+        if T == S:
+            return 1
+        if len(T) == len(S):
+            return 0
+        if T[-1] == 'A':
+            T = T[:-1]
             if T == S:
-                print(1)
-                return
-            if len(T) == 1:
-                print(0)
-                return
-            if T[-1] == 'A':
-                T = T[:-1]
-                if T == S:
-                    print(1)
-                    return
-                Q.append(T)
-            elif T[-1] == 'B':
-                T = T[:-1]
-                T = T[::-1]
-                if T == S:
-                    print(1)
-                    return
-                Q.append(T)
-    print(0)
-    return
+                return 1
+            Q.append(T)
+        else:
+            T = T[:-1][::-1]
+            if T == S:
+                return 1
+            Q.append(T)
 
-
-A = input()  # AB (2)
-B = input()  # ABB (3)
-Q = deque([B])  # Q에는 ABB
-check(A, B)
+s = input()
+t = input()
+Q = [t]
+print(check(s, t))
