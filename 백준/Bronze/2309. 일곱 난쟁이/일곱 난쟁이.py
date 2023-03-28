6e54a80157910580c6f0244c580a98c395491c64
Minkyoung Bae <hellobaemin@gmail.com>
2023-03-29 00:09:32 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,16 +1,15 @@
-def nCr(n, r, s):
+def nCr(n, r, s):   # n개에서 r개를 고르는 조합, s = 선택할 수 있는 구간의 시작
     if r == 0:
-        if sum(comb) == 100:
-            for gnome in comb:
+        if sum(heights) == 100:
+            for gnome in heights:
                 print(gnome)
             exit()
     else:
         for i in range(s, n-r+1):
-            comb[r-1] = A[i]
+            heights[r-1] = gnomes[i]
             nCr(n, r-1, i+1)
 
-N = 9
-A = [int(input()) for _ in range(N)]
-A.sort(reverse=True)
-comb = [0] * 7
+gnomes = [int(input()) for _ in range(9)]
+gnomes.sort(reverse=True)
+heights = [0] * 7
 nCr(9, 7, 0)
