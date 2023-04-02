7d3d1e503b075768255ee5169a564fb24f7972d2
Minkyoung Bae <hellobaemin@gmail.com>
2023-04-02 16:29:48 +0900

    [D3] Title: 햄버거 다이어트, Time: 193 ms, Memory: 62,540 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -3,7 +3,6 @@ def burger(i, sumK, taste):
     if sumK >= target:
         maxT = max(maxT, taste)
         return
-
     if i == N:
         if sumK >= target:
             maxT = max(maxT, taste)
@@ -16,8 +15,8 @@ for tc in range(1, int(input())+1):
     N, maxK = map(int, input().split())
     lst = [list(map(int, input().split())) for _ in range(N)]
     target = sum(list(k for _, k in lst)) - maxK
-    totalT = sum(list(t for t, _ in lst))
     lstK = sorted(lst, key=lambda x: (x[1], x[0]), reverse=True)
+    
     maxT = 0
-    burger(0, 0, totalT)
+    burger(0, 0, sum(list(t for t, _ in lst)))
     print(f'#{tc} {maxT}')
