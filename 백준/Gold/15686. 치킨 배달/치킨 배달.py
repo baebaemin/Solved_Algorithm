5c3cf169bd01365c387266b3d2d595bb86f0aa0d
Minkyoung Bae <hellobaemin@gmail.com>
2023-04-09 23:38:44 +0900

    [Gold V] Title: 치킨 배달, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -2,8 +2,7 @@ from itertools import combinations
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-houses = []
-chickens = []
+houses, chickens = [], []
 for i in range(N):
     for j in range(N):
         if arr[i][j] == 1:
@@ -11,31 +10,21 @@ for i in range(N):
         elif arr[i][j] == 2:
             chickens.append([i, j])
 
-H = len(houses)
-C = len(chickens)
+H, C = len(houses), len(chickens)
+
 table = [[0] * H for _ in range(C)]
 for i in range(C):
     for j in range(H):
         c, h = chickens[i], houses[j]
-        dist = abs(c[0]-h[0])+abs(c[1]-h[1])
-        table[i][j] = dist
+        table[i][j] = abs(c[0]-h[0]) + abs(c[1]-h[1])
 
-t_table = list(zip(*table))
-minV = 0
-for l in range(H):
-    minV += min(t_table[l])
+minV = sum(min(dist_lst)for dist_lst in zip(*table))
 survived = list(combinations(range(C), M))
 
 rlt = 0xffffff
 for s_lst in survived:
-    new_table = []
-    for s in s_lst:  # s는 row인덱스
-        new_table.append(table[s])
-
-    sm = 0
-    t_new_table = list(zip(*new_table))
-    for t in range(H):
-        sm += min(t_new_table[t])
+    new_table = [table[s] for s in s_lst]   # s는 row 인덱스
+    sm = sum(min(t_lst) for t_lst in zip(*new_table))
     rlt = min(sm, rlt)
     if rlt == minV:
         break
