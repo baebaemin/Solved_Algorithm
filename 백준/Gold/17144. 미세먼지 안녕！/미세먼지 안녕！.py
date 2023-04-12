fe3d45fff09172ac78c0e9f567a07ac956911c98
Minkyoung Bae <hellobaemin@gmail.com>
2023-04-13 01:12:47 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 2564 ms, Memory: 122644 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,13 +1,9 @@
 import sys; sys.setrecursionlimit(100000)
 
 def AirCleaner(t, arr):
-    if t == T:
-        rlt = 0
-        for i in range(R):
-            rlt += sum(arr[i])
-        return print(rlt + 2)
-
+    if t == T: return print(sum(map(sum, arr)) + 2)
     t_arr = [[0] * C for _ in range(R)]
+    
     ar = 0
     for r in range(R):
         for c in range(C):
@@ -16,7 +12,7 @@ def AirCleaner(t, arr):
                 if mid == -1:
                     t_arr[r][c] = -1
                     ar = r
-                else:   # -1을 제외한 숫자
+                else:   # 공기청정기가 아닌 곳
                     sm = 0
                     for dr, dc in ((-1, 0), (0, 1), (1, 0), (0, -1)):
                         nr, nc = r+dr, c+dc
@@ -25,23 +21,15 @@ def AirCleaner(t, arr):
                             sm += mid // 5
                     t_arr[r][c] += mid-sm
 
-    for ur in range(ar-3, -1, -1):  # 윗부분 먼지 빨아들여
-        t_arr[ur+1][0] = t_arr[ur][0]
-    for uc in range(1, C):
-        t_arr[0][uc-1] = t_arr[0][uc]
-    for ur in range(1, ar):
-        t_arr[ur-1][C-1] = t_arr[ur][C-1]
-    for uc in range(C-2, -1, -1):
-        t_arr[ar-1][uc+1] = t_arr[ar-1][uc]
-
-    for lr in range(ar+2, R):       # 아부분 먼지 빨아들여
-        t_arr[lr-1][0] = t_arr[lr][0]
-    for lc in range(1, C):
-        t_arr[R-1][lc-1] = t_arr[R-1][lc]
-    for lr in range(R-2, ar-1, -1):
-        t_arr[lr+1][C-1] = t_arr[lr][C-1]
-    for lc in range(C-2, 0, -1):
-        t_arr[ar][lc+1] = t_arr[ar][lc]
+    # 먼지 청소..하드..코딩...
+    for ur in range(ar-3, -1, -1):  t_arr[ur+1][0] = t_arr[ur][0]
+    for uc in range(1, C):          t_arr[0][uc-1] = t_arr[0][uc]
+    for ur in range(1, ar):         t_arr[ur-1][C-1] = t_arr[ur][C-1]
+    for uc in range(C-2, -1, -1):   t_arr[ar-1][uc+1] = t_arr[ar-1][uc]
+    for lr in range(ar+2, R):       t_arr[lr-1][0] = t_arr[lr][0]
+    for lc in range(1, C):          t_arr[R-1][lc-1] = t_arr[R-1][lc]
+    for lr in range(R-2, ar-1, -1): t_arr[lr+1][C-1] = t_arr[lr][C-1]
+    for lc in range(C-2, 0, -1):    t_arr[ar][lc+1] = t_arr[ar][lc]
 
     t_arr[ar - 1][1] = t_arr[ar][1] = 0
     AirCleaner(t + 1, t_arr)
