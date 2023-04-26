f84146a8da1ae90af7508a3c786e1e122d346c4f
Minkyoung Bae <hellobaemin@gmail.com>
2023-04-26 23:17:52 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,11 +1,5 @@
 while True:
     line = input()
-    if line == '0':
-        break
-    else:
-        half_l = half_r = len(line) // 2
-        if len(line) % 2:
-            half_r += 1
-        R_line = ''.join(reversed(list(line[half_r:])))
-        if line[:half_l] == R_line: print('yes')
-        else: print('no')
+    if line == '0':         break
+    if line == line[::-1]:  print('yes')
+    else:                   print('no')
