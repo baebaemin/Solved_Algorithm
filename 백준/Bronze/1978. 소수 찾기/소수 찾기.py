c33a14796dfd63b07d41d72af99e00afb9047844
Minkyoung Bae <hellobaemin@gmail.com>
2023-05-16 00:17:20 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,17 +1,11 @@
+def findPrime(num):
+    if num <= 1 or (num > 2 and num % 2 == 0):
+        return False
+    for divnum in range(3, num // 2, 2):
+        if not num % divnum:
+            return False
+    return True
+
 N = int(input())
 arr = list(map(int, input().split()))
-cnt = 0
-for num in arr:
-    if num == 2 or num == 3:
-        cnt += 1
-        continue
-    if num <= 1 or not num % 2:  # 소수가 아님
-        continue
-    divider = 3
-    while divider <= num // 2 :
-        if not num % divider:   # 소수가 아님
-            break
-        divider += 2
-    else:
-        cnt += 1
-print(cnt)
+print(sum([findPrime(n) for n in arr]))
