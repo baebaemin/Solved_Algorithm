9a861ee4db269ae157b67804cbe882139361b83d
Minkyoung Bae <hellobaemin@gmail.com>
2023-06-17 23:32:33 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,11 +1,10 @@
 def solution(players, callings):
-    p_dict = {}
-    for i, player in enumerate(players):
-        p_dict[player] = i
+    p_dict = {player: i for i, player in enumerate(players)}
+    
     for called in callings:
         c = p_dict[called]
         p_dict[called] -= 1
         p_dict[players[c-1]] += 1
         players[c-1], players[c] = players[c], players[c-1]
-    answer = players
-    return answer
+
+    return players
