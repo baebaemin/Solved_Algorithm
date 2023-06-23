4bd37a662dc93c0bcefccdfe34330771baf16984
Minkyoung Bae <hellobaemin@gmail.com>
2023-06-23 18:04:05 +0900

    [Silver II] Title: 랜선 자르기, Time: 432 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,37 +1,16 @@
 K, N = map(int, input().split())
-lst = [int(input()) for _ in range(K)]
+wires = [int(input()) for _ in range(K)]
+sp = 1
+ep = max(wires)
 
-
-def divider(s, m, e):  # 1, 229, 457
-    if (e - s) <= 1:
-        cut_s = cut_e = 0
-        for i in range(K):
-            cut_s += lst[i] // s
-            cut_e += lst[i] // e
-        if cut_s > cut_e:
-            return s
-        else:
-            return e
-
-    cut = 0
-    for i in range(K):
-        cut += lst[i] // m
-
-    if cut >= N:
-        s = m
-        m = (m + e) // 2
+while sp <= ep:
+    m = (sp + ep) // 2
+    cnt = 0
+    for wire in wires:
+        cnt += wire // m
+    if cnt >= N:
+        ans = m
+        sp = m + 1
     else:
-        e = m
-        m = (s + m) // 2
-    return divider(s, m, e)
-
-
-ep = max(lst)
-cnt = 0
-for i in range(K):
-    cnt += lst[i] // ep
-if cnt >= N:
-    print(ep)
-else:
-    mp = (ep + 1) // 2
-    print(divider(1, mp, ep))
+        ep = m - 1
+print(ans)
