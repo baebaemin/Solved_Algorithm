e756e060862d95fdbd1be6638368aa7e574577d3
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-11-23 15:08:21 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -3,8 +3,9 @@ nyam = 0
 
 for mushroom in mushrooms:
     nyam += mushroom
-    if nyam >= 100:
-        if abs(100 - nyam) > abs(100 - (nyam - mushroom)):
+    if nyam > 100:
+        if nyam-100 > 100 - (nyam-mushroom):
             nyam -= mushroom
         break
+
 print(nyam)
