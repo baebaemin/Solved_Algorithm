5d1e95ba2c50d4fb60738147d3b4395c2293da46
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-12-04 22:39:51 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 46.51 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,46 +1,29 @@
 from collections import deque
-
-def bfs(i, j, H, W, land, visited, cnt_dict, idx):
-    q = deque()
-    q.append((i, j))
-    visited[i][j] = idx
-    cnt = 1
-    
-    while q:
-        ci, cj = q.popleft()
-        for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            ni, nj = di+ci, dj+cj
-            if 0 <= ni < H and 0 <= nj < W and land[ni][nj] == 1 and not visited[ni][nj]:
-                visited[ni][nj] = idx
-                cnt += 1
-                q.append((ni, nj))
-    cnt_dict[idx] = cnt
-    idx += 1
-            
+  
 def solution(land):
-    idx = 1
-    H = len(land)    # 세로
-    W = len(land[0]) # 가로
+    H, W = len(land), len(land[0])  # 세로랑 가로
     visited = [[0] * W for _ in range(H)]
-    cnt_dict = {}
+    oil_value = [0] * W
     
     for i in range(H):
         for j in range(W): 
             if land[i][j] and not visited[i][j]:
-                bfs(i, j, H, W, land, visited, cnt_dict, idx)
-                idx += 1
-                
-    visited = list(map(list, zip(*visited)))  # 전치행렬
-    lst = [[] for _ in range(W)]
-    for i, l in enumerate(visited):
-        lst[i] = set(l)
-    unique_lst = list(map(set, set(map(frozenset, lst))))
-    
-    maxV = 0
-    for unique_set in unique_lst:
-        cnt = 0
-        for u in unique_set:
-            if u:
-                cnt += cnt_dict[u]
-        maxV = max(cnt, maxV)
-    return maxV
+                land_width = set()
+                q = deque()
+                q.append([i, j])
+                visited[i][j] = 1
+                oil_cnt = 1
+                land_width.add(j)
+                while q:
+                    ci, cj = q.popleft()
+                    for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                        ni, nj = di+ci, dj+cj
+                        if 0 <= ni < H and 0 <= nj < W and land[ni][nj] == 1 and not visited[ni][nj]:
+                            visited[ni][nj] = 1
+                            q.append((ni, nj))
+                            land_width.add(nj)
+                            oil_cnt += 1
+            
+                for o in land_width:
+                    oil_value[o] += oil_cnt
+    return max(oil_value)
