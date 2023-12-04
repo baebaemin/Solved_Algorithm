e4af722743ae6a138b9241041de9820ab0b3e450
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-12-05 01:11:17 +0900

    [level 3] Title: 가장 먼 노드, Time: 23.56 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,27 +1,25 @@
-def bfs(adj, n):
-    q = []  # 넣을 곳
+from collections import deque
+
+def bfs(n, adj):
     v = [0] * (n+1)
-    
-    q.append(1) # 시작점부터 넣기
+    q = deque([1])
     v[1] = 1
     
     while q:
-        c = q.pop(0)    # q에서 꺼낸 것. 맨 처음엔 1
-        for a in adj[c]:
-            if not v[a]: # 방문 안 했을 때
-                q.append(a)     # 다음 방문 시작지점에 넣음
-                v[a] = v[c] + 1 # 이동한 거리만큼 플러스
-            # 방문 했을 땐 패스
-            
-    rlt = v.count(max(v))
-    return rlt
+        now = q.popleft()
+        for next in adj[now]:
+            if not v[next]:
+                v[next] = v[now] + 1
+                q.append(next)
+                
+    return v.count(max(v))
 
 def solution(n, edge):
-    # 시작할 노드는 1 -> 0이라고 할까?
-    s = 0
-    # 간선 개수만큼 주기
     adj = [[] for _ in range(n+1)]
-    for e in edge:
-        adj[e[0]].append(e[1])
-        adj[e[1]].append(e[0])
-    return bfs(adj, n)
+    
+    # 양방향
+    for s, e in edge:
+        adj[s].append(e)
+        adj[e].append(s)
+        
+    return bfs(n, adj)
