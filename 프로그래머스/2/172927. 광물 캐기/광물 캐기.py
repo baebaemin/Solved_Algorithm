64c80f7df9e0e2fa542738d70edc7197f5883748
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-12-13 19:09:26 +0900

    [level 2] Title: 광물 캐기, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,40 +1,28 @@
 def solution(picks, minerals):
-    minerals += [''] * 5
-    picks_num = sum(picks)
-    section_lst = []
-    tired = 0
+    # 광물 리스트를 5개 단위로 분할하기 위함
+    minerals.extend([''] * (5 - len(minerals) % 5))
     
-    for i in range(picks_num):        
-        minerals_section = minerals[i*5:i*5+5]
-        if not minerals_section or minerals_section == ['', '', '']:
-            break
-            
-        diamond_cnt = minerals_section.count('diamond')
-        iron_cnt = minerals_section.count('iron')
-        stone_cnt = minerals_section.count('stone')
-        section_lst.append([diamond_cnt, iron_cnt, stone_cnt])
-    
-    section_lst.sort(key=lambda x: (x[0], x[1]), reverse=True)
+    # 5개 단위로 분할한 섹션별 광물 갯수 배열에 저장
+    mineral_counts = []
+    for i in range(sum(picks)):
+        section = minerals[i*5:i*5+5]
+        mineral_counts.append([section.count('diamond'), section.count('iron'), section.count('stone')])
 
-    while picks[0] and section_lst:
-        picks[0] -= 1
-        section = section_lst.pop(0)
-        tired += sum(section)
+    # 다이아몬드, 철 갯수 기준으로 내림차순 정렬
+    mineral_counts.sort(key=lambda x: (x[0], x[1]), reverse=True)
     
-    while picks[1] and section_lst:
-        picks[1] -= 1
-        section = section_lst.pop(0)
-        tired += 5 * section[0]
-        tired += section[1]
-        tired += section[2]
-    
-    while picks[2] and section_lst:
-        picks[2] -= 1
-        section = section_lst.pop(0)
-        tired += 25 * section[0]
-        tired += 5 * section[1]
-        tired += section[2]
-    
+    tired = 0
+    for pick_type, pick_cnt in enumerate(picks):    # 종류(idx)랑 개수
+        for _ in range(pick_cnt):                   # 곡괭이 수만큼 반복
+            if mineral_counts:
+                section = mineral_counts.pop(0)
+                if pick_type == 0:
+                    tired += sum(section)
+                elif pick_type == 1:
+                    tired += 5 * section[0] + sum(section[1:])
+                elif pick_type == 2:
+                    tired += 25 * section[0] + 5 * section[1] + section[2]
+            
     return tired
 
     
