12a9d583052853a8690823465c6197172eb65f50
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2024-01-06 00:00:25 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 128 ms, Memory: 11108 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -1,28 +1,12 @@
-const readline = require("readline");
+let fs = require('fs');
+let [N,K] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 
-let N, K;
+const peoples = Array.from({ length: N }, (_, i) => i + 1);
+const ans = [];
+let next = K - 1;
 
-(async () => {
-  let rl = readline.createInterface({ input: process.stdin });
-
-  for await (const line of rl) {
-    [N, K] = line.split(" ").map(Number);
-    let peoples = Array.from({ length: N }, (v, i) => i + 1);
-    let ans = [];
-    let next;
-    if (K % N - 1 >= 0) {
-        next = K % N - 1;
-    } else {
-        next = K - 1;
-    }
-
-    while (peoples.length > 0) {
-      ans.push(peoples.splice(next, 1)); // 형변환
-      next = (next + K - 1) % (peoples.length);
-    }
-    console.log('<'+ans.join(', ')+'>');
-    rl.close();
-  }
-
-  process.exit();
-})();
+while (peoples.length > 0) {
+  ans.push(peoples.splice(next, 1));
+  next = (next + K - 1) % peoples.length;
+}
+console.log(`<${ans.join(', ')}>`);
