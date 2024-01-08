d22e2f29a4a5b35577a05482dbf26b5f5288a17c
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2024-01-08 22:11:02 +0900

    [level 2] Title: 프로세스, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,20 +1,12 @@
 function solution(priorities, location) {
-    let enumPriorities = priorities.map((val, idx) => [idx, val])  
-    let rlt = [];
+    let enumPriorities = priorities.map((val, idx) => [idx, val])  // enumerate
+    let queue = [];
     
     while (enumPriorities.length > 0) {
         let curV = enumPriorities.shift()
-        
-        // [1] 뽑은 값이 지금 있는 애들 중에 가장 우선순위가 높지 않다면 -> 같은 값이면?
-        if (enumPriorities.find(val => val[1] > curV[1])) { // true라면? 가장 높지 않음
-            enumPriorities.push(curV)
-        } else {
-            rlt.push(curV)
-        }
+        // 우선순위가 더 높은 프로세스가 없다면(false) queue에 push
+        enumPriorities.find(val => val[1] > curV[1]) ? enumPriorities.push(curV) : queue.push(curV)
     }
     
-    // console.log(rlt)
-    // console.log()
-    // var answer = 0;
-    return rlt.findIndex(val => val[0] === location) + 1;
+    return queue.findIndex(val => val[0] === location) + 1;
 }
