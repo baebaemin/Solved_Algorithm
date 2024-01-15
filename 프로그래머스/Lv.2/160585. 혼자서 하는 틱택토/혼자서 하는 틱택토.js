053ec979f166e382b040acf33e87074131792aa3
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2024-01-15 21:39:02 +0900

    [level Lv.2] Title: 혼자서 하는 틱택토, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.js
@@ -0,0 +1,85 @@
+// 불가능 조건 [1] : cntO < cntX
+// 불가능 조건 [2] : cntO > cntX + 1
+// 불가능 조건 [3] : winO && winX <- 판별할 때 cntO랑 xntX 수가 둘다 3 이상이면
+
+function solution(board) {
+    // O 개수랑 X 개수 세기
+    let ans;
+    let cntO = 0;
+    let cntX = 0;
+    let winO = false;
+    let winX = false;
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
+            if (board[r][c] === 'O') {
+                cntO++;
+            } else if (board[r][c] === 'X') {
+                cntX++;
+            }
+        }
+    }
+    
+    if (cntO < cntX || cntO > cntX + 1) {
+        return 0
+    }
+    
+    if (cntO >= 3 && cntX >= 3) {
+        for (let x = 0; x < 3; x++) {
+            let widthStateO = 0;
+            let widthStateX = 0;
+            let heightStateO = 0;
+            let heightStateX = 0;
+            
+            for (let y = 0; y < 3; y++) {
+                if (board[x][y] === 'O') {  // 가로
+                    widthStateO++;
+                } else if (board[x][y] === 'X') {
+                    widthStateX++;
+                }
+                
+                if (board[y][x] === 'O') {  // 세로
+                    heightStateO++;
+                } else if (board[y][x] === 'X') {
+                    heightStateX++;
+                }
+            }
+            if (widthStateO === 3 || heightStateO === 3) {
+                winO = true;
+            } else if (widthStateX === 3 || heightStateX === 3) {
+                winX = true;
+            }
+        }
+        
+        // 대각선 판별 - 우하향
+        if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+            if (board[1][1] === 'O') {
+                winO = true;
+            } else if (board[1][1] === 'X') {
+                winX = true;
+            }
+        }
+        
+        // 대각선 판별 - 우상향
+        if (board[2][0] === board[1][1] && board[1][1] === board[0][2]) {
+            if (board[1][1] === 'O') {
+                winO = true;
+            } else if (board[1][1] === 'X') {
+                winX = true;
+            }
+        }
+        
+        if (winO === true && winX === true) {
+            return 0
+        }
+    }
+    
+    if (cntO > cntX && winO === false && winX === true) {
+        return 0
+    }
+    
+    if (cntO <= cntX && winO === true && winX === false) {
+        return 0
+    }
+    
+    return 1
+}
