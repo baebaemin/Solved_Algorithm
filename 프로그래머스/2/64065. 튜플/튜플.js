e70882c28d2f7140f0ea2f4b5193afb0c4f1026e
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2024-02-26 22:36:27 +0900

    [level 2] Title: 튜플, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,19 +1,17 @@
 function solution(s) {
     const answer = [];
-    const array = s.slice(2, s.length-2).split('},{');
-    const N = array.length;
+    const array = s.slice(2, s.length-2).split('},{').map(str => str.split(',').map(Number));
     array.sort((x, y) => x.length - y.length)
     
-    for (let i = 0; i < N; i++) {
-        const miniArray = array[i].split(',');
-        
-        miniArray.map((elem) => {
-            let n = Number(elem)
-            if (answer.includes(n) === false) {
-                answer.push(n)
+    const checkSet = new Set(); // 중복 제거용
+    array.forEach(item => {
+        item.forEach(num => {
+            if (!checkSet.has(num)) {
+                answer.push(num);
+                checkSet.add(num);
             }
-        })
-    }
+        });
+    });
     
     return answer;
 }
